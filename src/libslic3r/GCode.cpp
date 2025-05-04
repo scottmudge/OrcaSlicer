0c9b2a583ff9a44cf89a62ebe2f320bed04d294d
Scott Mudge <mail@scottmudge.com>
2025-05-04 12:00:45 -0400

    .

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -5285,7 +5285,7 @@ std::string GCode::_extrude(const ExtrusionPath &path, std::string description, 
     // calculate effective extrusion length per distance unit (e_per_mm)
     double filament_flow_ratio = m_config.option<ConfigOptionFloats>("filament_flow_ratio")->get_at(0);
     // We set _mm3_per_mm to effectove flow = Geometric volume * print flow ratio * filament flow ratio * role-based-flow-ratios
-    auto _mm3_per_mm = path.mm3_per_mm * this->config().print_flow_ratio;
+    auto _mm3_per_mm = path.mm3_per_mm * this->config().print_flow_ratio * path.extrusion_multiplier;
     _mm3_per_mm *= filament_flow_ratio;
     if (path.role() == erTopSolidInfill)
         _mm3_per_mm *= m_config.top_solid_infill_flow_ratio;
